fe55b81bcec07063b01b616f66fe813d8603cb37
Bontobox <leesimonbonto@gmail.com>
2023-12-16 00:05:10 -0700

    Final commit

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,24 +3,24 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
-var cost_per_day=0;
+var cost_per_day = 0;
 var HalfFull = "Full";
 
-
+var document = '';
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
-var mon=document.getElementById("monday");
-var tue=document.getElementById("tuesday");
-var wed=document.getElementById("wednesday");
-var thur=document.getElementById("thursday");
-var fri=document.getElementById("friday");
+var mon = document.getElementById("monday");
+var tue = document.getElementById("tuesday");
+var wed = document.getElementById("wednesday");
+var thur = document.getElementById("thursday");
+var fri = document.getElementById("friday");
 
-var clear=document.getElementById("clear-button");
+var clear = document.getElementById("clear-button");
 function add_days(rate){
-    var count =0;
-    var button =document.getElementById("day")
+    var count = 0;
+    var button = document.getElementById("day")
     if(this.classList.contains("day")){
         this.classList.add('clicked'); 
     }
@@ -55,23 +55,23 @@ clear.addEventListener('click',clear_days);
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
-var full=document.getElementById("full");
-var half=document.getElementById("half");
+var full = document.getElementById("full");
+var half = document.getElementById("half");
 function Rates(){;
     var rate =0;
     if(half.classList.contains('clicked')){
         full.classList.add('clicked');
         half.classList.remove('clicked');
-        rate =35;
+        rate = 35;
         recalculate(count,rate);
-        return rate
+        return rate;
     }
     else{
         half.classList.add('clicked');
         full.classList.remove('clicked');
-        rate =20;
+        rate = 20;
         recalculate(count,rate);
-        return rate
+        return rate;
     }
 }
 half.addEventListener('click',Rates);
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,12 +1,14 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
-
+"use strict";
+var document = '';
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-var submit =document.getElementById("submit-button")
-var main =document.getElementById("contact-page")
-function display(){
-    if(submit.classList.contains("submit")){
+var submit = document.getElementById("submit-button");
+var main = document.getElementById("contact-page");
+
+function display() {
+    if (submit.classList.contains("submit")) {
         main.classList.add('nodisplay');
-        main.innerHTML ="Thank you for your Message!"
+        main.innerHTML = "Thank you for your Message!";
     }
 }
-submit.addEventListener("click",display)
+submit.addEventListener("click", display);
